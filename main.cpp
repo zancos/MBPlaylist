457404df6a2948e57db1180052bc001c4c15d61a
Fernando Castaño <zancos@hotmail.com>
2017-08-05 10:49:52 +0200

    Test with writting hex file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <algorithm>
 #include "songList.h"
 
 
@@ -14,20 +15,32 @@ int main()
     song.setSong(std::string("\\\\TORTUGASNINJA\\OpenShare\\Music\\El Canto del Loco\\2005 Zapatillas\\05 Besos.mp3"));
     songList.pushSong(song);
 
-    song.setSong(std::string("\\\\TORTUGASNINJA\\OpenShare\\Music\\fun_\\2012 Some Nights\\03 fun. feat. Janelle Monáe - We Are Young.mp3"));
+    song.setSong(std::string(
+        "\\\\TORTUGASNINJA\\OpenShare\\Music\\fun_\\2012 Some Nights\\03 fun. feat. Janelle Monáe - We Are Young.mp3"));
     songList.pushSong(song);
 
-    song.setSong(std::string("\\\\TORTUGASNINJA\\OpenShare\\Music\\Los Piratas\\1993 Quiero hacerte gritar\\06 El sabor de las cosas.mp3"));
+    song.setSong(std::string(
+        "\\\\TORTUGASNINJA\\OpenShare\\Music\\Los Piratas\\1993 Quiero hacerte gritar\\06 El sabor de las cosas.mp3"));
     songList.pushSong(song);
 
     ofstream f;
-    f.open("pruebastd.mbp", ios::binary);
+    f.open("pruebastd2.mbp", ios::binary);
 
-
-    for (int i = 0; i < songList.size(); i++)
+    std::vector<Song> songListVector = songList.getSongList();
+    std::reverse(songListVector.end(), songListVector.begin());
+    for (Song s : songListVector)
     {
-        f << songList.getIndexSong(i).getSizeOf();
-        f << songList.getIndexSong(i).getSong();
+        f << s.getSong();
+        f << s.getSizeOf();
+        int32_t n = 0xFFFFFFFF;
+        char data[4];
+        data[0] = static_cast<char>(n & 0xFF);
+        data[1] = static_cast<char>((n >> 8) & 0xFF);
+        data[2] = static_cast<char>((n >> 16) & 0xFF);
+        data[3] = static_cast<char>((n >> 24) & 0xFF);
+        f.write(data, 4);
+
+
     }
 
     /*
